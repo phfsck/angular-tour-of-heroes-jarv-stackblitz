70624a3d6b7d1a961dd9f6c1e029eb4e0dc501fa
phfsck <coeus.ph@gmail.com>
2019-10-10 16:57:31 +0800

    Additional Hero Detail component.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { HEROES } from '../assets/mock-heroes';
+import { HEROES } from '../../assets/mock-heroes';
 import { Hero } from '../hero';
 
 @Component({
