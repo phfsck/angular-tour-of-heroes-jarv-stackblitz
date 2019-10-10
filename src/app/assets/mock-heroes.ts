dc164e6cff5ee3210fbb150f43baff85055297ea
phfsck <coeus.ph@gmail.com>
2019-10-10 16:34:40 +0800

    Hero editor and displaying of a list exercise.

---
+++ src/app/assets/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr. Nicey' },
+  { id: 12, name: 'Machoman' },
+  { id: 13, name: 'Boombastic' },
+  { id: 14, name: 'Celery' },
+  { id: 15, name: 'Magneto' },
+  { id: 16, name: 'Rubberman Bitoy' },
+  { id: 17, name: 'Dynamo' },
+  { id: 18, name: 'Dr. Hu' },
+  { id: 19, name: 'Magmaman' },
+  { id: 20, name: 'Tornado' }
+];
